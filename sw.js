a9dca0c8bfc0cb1212406a9482903469cb54c31f
max-crashed <e.g.romanowska@gmail.com>
2025-11-03 14:05:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html","style/style.css"]); toolbox.router.get('images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
 const CACHE_NAME = 'max-app-v1';
 const urlsToCache = [
   '/',
